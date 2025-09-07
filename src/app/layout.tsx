8467b1840b5cc9dfeb24a463dbabbb3b53e0c58b
Felicia Odewabi <odewabifd@gmail.com>
2025-09-07 08:01:40 +0000

    Change the name title to Menda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
-  title: 'Artisan Connect',
+  title: 'Menda',
   description: 'Connect with skilled artisans in Nigeria for your service needs.',
   keywords: 'artisans, Nigeria, plumber, electrician, carpenter, services marketplace',
 };
